1526e9deae7d9b9acf5c6f78ad115e7da7389d1b
Selvakumar <Selvakumar.A@tesco.com>
2023-08-08 09:33:21 +0530

    [JIRA-1001] - Initial Commit

---
+++ src/main/java/com/example/gitspringdemo/GitSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSpringDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitspringdemo/GitSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
